a6e178a7657e354d177f532312fb9be231581763
Sakshi Lumba <sakshilumba@gmail.com>
2020-06-15 16:18:01 +0530

    testing Jenkins latest

--- src/test/java/WebdriverMaven/testLogin.java
+++ src/test/java/WebdriverMaven/testLogin.java
@@ -36,7 +36,7 @@ public class testLogin {
 	@AfterSuite
 	public void teardown() {
 		/*
-		 * checking the changes in Jenkins latest
+		 * checking the changes in Jenkins latest1
 		 */
 		
 		driver.quit();
